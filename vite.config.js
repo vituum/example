ffd1350d46baa244c913f4351c6a39561a1852c0
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-08-21 20:07:58 +0200

    feat: updated example

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vituum'
 import { resolve } from 'path'
+import latte from '@vituum/latte'
+import juice from '@vituum/juice'
+import posthtml from '@vituum/posthtml'
+import tailwind from '@vituum/tailwind'
 
 export default defineConfig({
+    integrations: [juice(), posthtml(), tailwind(), latte({
+        globals: {
+            template: resolve(process.cwd(), 'src/templates/latte/Layout/Main.latte')
+        }
+    })],
+    templates: {
+      format: `latte`
+    },
     server: {
         https: true
-    },
-    templates: {
-        latte: {
-            globals: {
-                template: resolve(process.cwd(), 'src/templates/latte/Layout/Main.latte')
-            }
-        }
     }
 })
